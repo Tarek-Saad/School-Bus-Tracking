0e1ba5b1ac2502a57e6c84faa62b99c4a5a79a39
Tarek-Saad <saadfouad1976t@gmail.com>
2025-10-25 18:13:48 +0300

    remove header

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,4 +1,3 @@
-import { Header } from "./header";
 import { Footer } from "./footer";
 
 interface LayoutProps {
@@ -8,7 +7,6 @@ interface LayoutProps {
 export function Layout({ children }: LayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
-      <Header />
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
